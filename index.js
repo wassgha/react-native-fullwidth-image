55287edae7553de6d3a428544b304081d1c8c59e
Wassim Gharbi <wassgha@gmail.com>
2019-02-19 17:04:43 -0500

    Bumped version and fixed android support

--- index.js
+++ index.js
@@ -9,66 +9,73 @@ import React, { Component } from 'react'
 import { View, Image, StyleSheet } from 'react-native'
 
 export default class FullWidthImage extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props)
 
-        this.state = {
-            width: props.width || null,
-            height: props.height || null
-        }
+    this.state = {
+      width: props.width || null,
+      height: props.height || null
     }
+  }
 
-    setNativeProps(nativeProps) {
-      this._root.setNativeProps(nativeProps);
+  setNativeProps(nativeProps) {
+    this._root.setNativeProps(nativeProps)
+  }
+
+  _onLayout(event) {
+    const containerWidth = event.nativeEvent.layout.width
+
+    if (this.props.ratio) {
+      this.setState({
+        width: containerWidth,
+        height: containerWidth * this.props.ratio
+      })
+    } else if (this.props.width && this.props.height) {
+      this.setState({
+        width: containerWidth,
+        height: containerWidth * (this.props.height / this.props.width)
+      })
+    } else if (this.props.source) {
+      let source = this.props.source
+      if (typeof source !== 'string') {
+        source = this.props.source.uri
+      }
+      Image.getSize(source, (width, height) => {
+        this.setState({
+          width: containerWidth,
+          height: (containerWidth * height) / width
+        })
+      })
     }
+  }
 
-    _onLayout(event) {
-        const containerWidth = event.nativeEvent.layout.width
-
-        if (this.props.ratio) {
-            this.setState({
-                width: containerWidth,
-                height: containerWidth * this.props.ratio
-            })
-        } else if (this.props.width && this.props.height) {
-          this.setState({
-              width: containerWidth,
-              height: containerWidth * (this.props.height / this.props.width)
-          })
-        } else if (this.props.source && this.props.source.uri) {
-            Image.getSize(this.props.source.uri, (width, height) => {
-                this.setState({
-                    width: containerWidth,
-                    height: containerWidth * height / width
-                })
-            })
-        }
-    }
-
-    render() {
-        return (
-            <View
-                ref={component => this._root = component}
-                onLayout={this._onLayout.bind(this)}
-                style={styles.container}
-            >
-                <Image
-                    source={this.props.source}
-                    style={[this.props.style, {
-                        width: this.state.width,
-                        height: this.state.height
-                    }]}
-                    onLoad={this.props.onLoad}
-                    onLoadEnd={this.props.onLoadEnd}
-                    onLoadStart={this.props.onLoadStart}
-                />
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View
+        ref={component => (this._root = component)}
+        onLayout={this._onLayout.bind(this)}
+        style={styles.container}
+      >
+        <Image
+          source={this.props.source}
+          style={[
+            this.props.style,
+            {
+              width: this.state.width,
+              height: this.state.height
+            }
+          ]}
+          onLoad={this.props.onLoad}
+          onLoadEnd={this.props.onLoadEnd}
+          onLoadStart={this.props.onLoadStart}
+        />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     alignSelf: 'stretch'
   }
-});
+})
